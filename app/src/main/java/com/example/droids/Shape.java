4081e4e249f13e66bda9bfde2eecc263fc684f8f
Farah Abdullahi <fhabdull@ualberta.ca>
2025-09-23 21:02:08 -0600

    Add abstract Shape class

---
+++ app/src/main/java/com/example/droids/Shape.java
@@ -0,0 +1,14 @@
+package com.example.droids;
+
+public abstract class Shape {
+    protected int x;
+    protected int y;
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public abstract void draw(); // Example abstract method
+}
+
