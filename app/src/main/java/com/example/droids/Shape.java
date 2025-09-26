269e61bf351260710f6bf3bf083d9005e69f4421
Farah Abdullahi <fhabdull@ualberta.ca>
2025-09-26 13:49:32 -0600

    Update Shape.java

--- app/src/main/java/com/example/droids/Shape.java
+++ app/src/main/java/com/example/droids/Shape.java
@@ -3,7 +3,7 @@ package com.example.droids;
 public abstract class Shape {
     protected int x;
     protected int y;
-    protected String color = "red";
+    protected String color = "green";
 
     public Shape(int x, int y) {
         this.x = x;
